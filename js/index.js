fe0980c0d3d10d7c11dec7e81f1dd5cc99a6478b
esteban <etoledob@uoc.edu>
2024-03-11 22:59:47 +0100

    pruebas3

--- js/index.js
+++ js/index.js
@@ -1,21 +1,33 @@
-$(document).ready(function() {
-    // Cuando se hace clic en el botón "Añadir elemento"
-    $('#addCardButton').on('click', function() {
-      // 
-      // Crear un nuevo elemento "card"
-      const newCard = `
-        <div class="card" style="width: ;">
-            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Título de la tarjeta</h5>
-                <p class="card-text">Esta es una tarjeta de ejemplo para mostrar cómo agregar un nuevo elemento al listado.</p>
-                <a href="#" class="btn btn-primary">Ir a alguna parte</a>
-            </div>
-        </div>
-      `;
-  
-      // Agregar la nueva "card" al contenedor de tarjetas
-      $('#cardList').append(newCard);
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  // Espera a que el documento HTML esté completamente cargado antes de ejecutar el código dentro de la función.
 
+  // Obtiene el botón "Añadir elemento" por su ID
+  var addCardButton = document.getElementById('addCardButton');
+
+  // Agrega un listener de eventos al botón "Añadir elemento"
+  addCardButton.addEventListener('click', function () {
+    // Esta función se ejecutará cuando se haga clic en el botón "Añadir elemento"
+
+    // Crea un nuevo elemento "div" para representar la tarjeta
+    var newCard = document.createElement('div');
+
+    // Establece la clase CSS del nuevo elemento como "card"
+    newCard.className = 'col-md-3 col-xl-4';
+
+    // Define el contenido HTML del nuevo elemento utilizando la propiedad innerHTML
+    newCard.innerHTML = `
+    <div  class="p-4 text-center shadow-lg m-5 rounded-5 px-5 mx-0 mx-sm-2 px-sm-4 py-sm-0 my-sm-1" style="background: linear-gradient(171deg, var(--bs-pink) 0%, var(--bs-indigo) 100%), var(--bs-purple);width: 178px;margin: 10px;margin-left: 39px;margin-right: 40px;padding: 0px;padding-right: 15px;padding-left: 14px;"><img class="pt-2 w-50" src="swiftui.png" />
+    <h3 class="text-white text-center pt-2">Title</h3>
+    <p class="fw-bold pt-1 text-white p-0 m-0">Subtitle</p>
+    <p class="fw-light text-white m-0">Description</p>
+    <hr class="text-white my-sm-2" /><img class="py-sm-0 my-sm-2 mx-sm-3 px-sm-0" src="swiftui.png" style="width: 10%;" />
+</div>
+      `;
+
+    // Obtiene el contenedor de tarjetas por su ID
+    var cardList = document.getElementById('cardList');
+
+    // Agrega el nuevo elemento "div" (tarjeta) como hijo del contenedor de tarjetas
+    cardList.appendChild(newCard);
+  });
 });
