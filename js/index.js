442177e7dce54d8435988a50c0110c53a8c398d8
esteban <etoledob@uoc.edu>
2024-03-18 22:40:34 +0100

    Añadir nuevo proyecto, añadir prioritario y salvar en sessionStorage

--- js/index.js
+++ js/index.js
@@ -9,44 +9,16 @@ console.log(projectsDb);
 var contador = 1;
 document.addEventListener("DOMContentLoaded", function () {
   // Espera a que el documento HTML esté completamente cargado antes de ejecutar el código dentro de la función.
-  // Inicializa la aplicación con los proyectos cargados
-  inicializarApp(projectsDb);
+
   // Obtiene el botón "Añadir elemento" por su ID
   var addCardButton = document.getElementById("addCardButton");
 
   // Agrega un listener de eventos al botón "Añadir elemento"
   addCardButton.addEventListener("click", function () {
-    openModal();
-    /*
-    // Esta función se ejecutará cuando se haga clic en el botón "Añadir elemento"
-
-    // Crea un nuevo elemento "div" para representar la tarjeta
-    var newCard = document.createElement("div");
-
-    // Establece la clase CSS del nuevo elemento como "card"
-    newCard.className = "card";
-
-    // Define el contenido HTML del nuevo elemento utilizando la propiedad innerHTML
-
-    var numeroCaja = "caja" + contador; // Variable con el número de caja
-    newCard.id = numeroCaja;
-    newCard.innerHTML = `    
-    <div class="card-title">Proyecto ${contador}</div>
-    <div class="card-description">Descripción del proyecto con id ${numeroCaja}.</div>    
-    <div class="iconFlag"><i class="fas fa-star"></i></div>
-    `;
-    contador++;
-    // Obtiene el contenedor de tarjetas por su ID
-    var cardList = document.getElementById("cajapadre");
-
-    // Agrega el nuevo elemento "div" (tarjeta) como hijo del contenedor de tarjetas
-    cardList.appendChild(newCard);
-    // Tras un breve retraso, activar la clase 'show' para aplicar la transición
-    setTimeout(function () {
-      newCard.classList.add("show");
-    }, 10);
-    */
+    openModal();    
   });
+  // Inicializa la aplicación con los proyectos cargados
+  inicializarApp(projectsDb);
 });
 
 /* >>>>>>>>>>>>>>>>>>>>>>
@@ -60,6 +32,7 @@ function inicializarApp(proDb) {
   proDb.forEach(function (project) {
     var newCard = document.createElement("div");
     newCard.className = "card";
+
     // var numeroCaja = "caja" + contador++;
     // le pongo el id
     newCard.id = project.id;
@@ -69,15 +42,19 @@ function inicializarApp(proDb) {
     //Comprobar si es prioritario
     var colorPriority = checkPriorityColor(project.priority); // Llama a la función para obtener el color
     newCard.innerHTML = `    
-          <div class="card-title" >${project.name}</div>
-          <div class="card-description">${project.description} con id ${project.id}.</div>
-          <div class="iconFlag"><i class="fas fa-flag" style="color: ${colorPriority}"></i></div>
-      `;
+    <div class="card-title" >${project.name}</div>
+    <div class="card-description">${project.description} con id ${project.id}.</div>
+    <div class="iconFlag"><i class="fas fa-flag"></i></div>
+    `;
+    newCard.querySelector('.iconFlag .fas').style.color = colorPriority; // Aplica el color al ícono
+
     cardList.appendChild(newCard);
     // Tras un breve retraso, activar la clase 'show' para aplicar la transición
     setTimeout(function () {
       newCard.classList.add("show");
     }, 10);
+
+
 
     // Agregar evento de clic a los íconos favoritos de proyectos precargados
     // Busca el ícono favorito dentro de un nuevo proyecto
@@ -101,6 +78,14 @@ function inicializarApp(proDb) {
         }
       });
     }
+    newCard.querySelector('.iconFlag .fas').addEventListener('mouseover', () => {
+      // Change the button's background color
+      newCard.querySelector('.iconFlag .fas').style.color = 'rgb(8 112 245)';
+    });
+    newCard.querySelector('.iconFlag .fas').addEventListener('mouseout', () => {
+      // Change the button's background color
+      newCard.querySelector('.iconFlag .fas').style.color = checkPriorityColor(project.priority);
+    });
   });
 }
 
@@ -176,38 +161,115 @@ function captureFormValues(event) {
   var projectName = document.getElementById('name').value;
   var department = document.getElementById('department').value;
   var description = document.getElementById('description').value;
+  console.log("Nombre del proyecto: " + projectName);
+  console.log("Departamento: " + department);
+  console.log("Descripción: " + description);
 
   // Verificar el estado de validación de cada campo
-  var isNameValid = projectName.validity.valid;
-  var isDepartmentValid = department.validity.valid;
-  var isDescriptionValid = description.validity.valid;
+  var isNameValid = document.getElementById('name').validity.valid;  
+  var isDepartmentValid = document.getElementById('department').validity.valid;
+  var isDescriptionValid = document.getElementById('description').validity.valid;
+  console.log(document.getElementById('name').validity.valid);
 
   // Comprobar si todos los datos son válidos
   if (isNameValid && isDepartmentValid && isDescriptionValid) {
     //TODO
     // Añadir datos al array de sessionStorage
+    console.log("Nombre del proyecto: " + projectName);
+    console.log("Departamento: " + department);
+    console.log("Descripción: " + description);
+    
 
+    let newProject = {
+      id:projectsDb.length + 1,
+      name: projectName,
+      description: description,
+      department:department,
+      backgroundColor: "rgb(150,200,135)",
+      priority: 0,
+      status: 1
+    }
+    console.log('newProject :>> ', newProject);
+    projectsDb.push(newProject);
+    
     //TODO
     // Crear una nueva tarjeta
-
+    crearNuevaTarjetaProyecto(newProject);
+    //TODO
+    //Cerrar el modal
+    // Limpiar los valores de los campos del formulario
+    document.getElementById('name').value = '';
+    document.getElementById('department').value = '';
+    document.getElementById('description').value = '';
+    // Cerrar el modal después de capturar los valores
+    $('#exampleModal').modal('hide');
   } else {
     //TODO
     // Avisar al usuario que algún dato no es valido
+
   }
-  // Aquí puedes realizar las acciones necesarias con los valores capturados
-  console.log("Nombre del proyecto: " + projectName);
-  console.log("Departamento: " + department);
-  console.log("Descripción: " + description);
-
-
-  // Limpiar los valores de los campos del formulario
-  document.getElementById('name').value = '';
-  document.getElementById('department').value = '';
-  document.getElementById('description').value = '';
-  // Cerrar el modal después de capturar los valores
-  $('#exampleModal').modal('hide');
-  // Por ejemplo, puedes enviar los datos al servidor utilizando AJAX o realizar alguna otra operación.
+  
 }
 /* >>>>>>>>>>>>>>>>>>>>>>
  * CREAR NUEVA TARJETA PROYECTO *
  <<<<<<<<<<<<<<<<<<<<<<<<*/
+ function crearNuevaTarjetaProyecto(newProject) {
+  let cardList = document.getElementById("cajapadre");
+  var newCard = document.createElement("div");
+  newCard.className = "card";
+
+  // var numeroCaja = "caja" + contador++;
+  // le pongo el id
+  newCard.id = newProject.id;
+  // y el color de fondo
+  newCard.style.backgroundColor = newProject.backgroundColor;
+  console.log(newProject.backgroundcolor);
+  //Comprobar si es prioritario
+  var colorPriority = checkPriorityColor(newProject.priority); // Llama a la función para obtener el color
+  newCard.innerHTML = `    
+  <div class="card-title" >${newProject.name}</div>
+  <div class="card-description">${newProject.description} con id ${newProject.id}.</div>
+  <div class="iconFlag"><i class="fas fa-flag"></i></div>
+  `;
+  newCard.querySelector('.iconFlag .fas').style.color = colorPriority; // Aplica el color al ícono
+
+  cardList.appendChild(newCard);
+  // Tras un breve retraso, activar la clase 'show' para aplicar la transición
+  setTimeout(function () {
+    newCard.classList.add("show");
+  }, 10);
+
+  // Agregar evento de clic a los íconos favoritos de proyectos precargados
+  // Busca el ícono favorito dentro de un nuevo proyecto
+  var iconFlag = newCard.querySelector(".iconFlag .fas");
+  if (iconFlag) {// Verifica si se encontró un ícono favorito
+    iconFlag.addEventListener("click", function (event) {// Agrega un evento de clic al ícono favorito encontrado
+      // Detiene la propagación del evento para evitar que se propague a elementos superiores
+      event.stopPropagation();
+      // Objtengo el id del div padre en su nivel
+      var projectId = parseInt(event.target.parentElement.parentElement.id);
+      console.log('projectId :>> ', projectId);
+      // como un stream en Java
+      var projectIndex = projectsDb.findIndex(newProject => newProject.id === projectId);
+      console.log('projectIndex :>> ', projectIndex);
+      console.log('newProject.id :>> ', newProject.id);
+      if (projectIndex !== -1) {// si existe
+        // aplico condicional ternario
+        projectsDb[projectIndex].priority = (projectsDb[projectIndex].priority === 0) ? 1 : 0;
+        // actualizo la sessionStorage
+        sessionStorage.setItem('projectsdb', JSON.stringify(projectsDb));
+        // checkeo y cambio el color
+        event.target.style.color = checkPriorityColor(projectsDb[projectIndex].priority);
+      }
+    });
+  }
+  newCard.querySelector('.iconFlag .fas').addEventListener('mouseover', () => {
+    // Change the button's background color
+    newCard.querySelector('.iconFlag .fas').style.color = 'rgb(8 112 245)';
+  });
+  newCard.querySelector('.iconFlag .fas').addEventListener('mouseout', () => {
+    // Change the button's background color
+    newCard.querySelector('.iconFlag .fas').style.color = checkPriorityColor(newProject.priority);
+  });
+
+ }
