73b8bd7c2a8707781955dff3fde4fb036a5776f5
esteban <etoledob@uoc.edu>
2024-03-21 18:59:22 +0100

    Columnas en interface tareas

--- js/interface-2.js
+++ js/interface-2.js
@@ -2,7 +2,26 @@
 /* >>>>>>>>>>>>>>>>>>>>>>
  * Recuperar datos de session *
  <<<<<<<<<<<<<<<<<<<<<<<<*/
-var getDataStorageProjects = sessionStorage.getItem('projectsdb');
+var getDataStorageProjects = localStorage.getItem('projectsdb');
 var projectsDb = JSON.parse(getDataStorageProjects);
-console.log('MOdal projectsDB :>> ', projectsDB);
+console.log('MOdal projectsDb :>> ', projectsDb);
 
+/* >>>>>>>>>>>>>>>>>>>>>>
+ * DRAG&DROP *
+ <<<<<<<<<<<<<<<<<<<<<<<<*/
+ function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  if(ev.target.classList.contains("comlumna-tareas")){
+      ev.target.appendChild(document.getElementById(data));
+  }
+
+}
